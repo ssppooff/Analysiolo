13bfb31b231c71b00092742631d14e83b13503d5
Raphael Cottier <github.zing@dfgh.net>
2022-09-27 17:06:41 +0200

    Added toString(), hashCode(), and equals() to Tuple.java

--- src/main/java/functionalUtilities/Tuple.java
+++ src/main/java/functionalUtilities/Tuple.java
@@ -8,4 +8,23 @@ public class Tuple<A, B> {
     this._1 = _1;
     this._2 = _2;
   }
+
+  @Override
+  public String toString() {
+    return String.format("(" + _1 + ", " + _2 + ")");
+  }
+
+  @Override
+  public int hashCode() {
+    return 1 + _1.hashCode() + _2.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if ( !(obj instanceof Tuple<?, ?> that) )
+      return false;
+
+    return _1.equals(that._1)
+        && _2.equals(that._2);
+  }
 }
