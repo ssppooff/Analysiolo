7048ff53a15eec9a7b40b2f502d821db7a821c41
Raphael Cottier <github.zing@dfgh.net>
2022-10-10 14:23:41 +0200

    Fixed and refactored MainTest.java except for H2 database methods.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import functionalUtilities.DataBase;
 import functionalUtilities.FileReader;
+import functionalUtilities.List;
 import functionalUtilities.Map;
 import functionalUtilities.Result;
 import functionalUtilities.Stream;
@@ -13,26 +16,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
-import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import stockAPI.Symbol;
 import stockAPI.Transaction;
 
-/** TODO
- ** Current task & subtasks:
- * Stream of rows of ResultSet
- ** Stream.java -> flattenResult
- - Input all the transactions into db
- - Connect to H2 database using JDBC, write & read stuff to it
- - ?? db stuff
- - check whether txType and price (pos or neg) corresponds
- - nShares integer or double or BiDecimal?
+/* TODO Current task & subtasks:
+    - nShares integer or double or BigDecimal?
+    - ~~Input all the transactions into db~~
+    - ~~Write methods to write multiple rows into db~~
+    - ~~Write methods to read multiple rows into db~~
 
  If no date is given/what is the value of the portfolio now?
  * Input all the transactions into db
- - Read all the transactions from the db into memory
+ * Read all the transactions from the db into memory
  - ask Yahoo Finance the prices of the list
- - ~compile list of all stock held at this current  moment~
 
  If the user asks for the value of the portfolio at the end of a certain date
  - figure out what stocks were held at the end of that day
@@ -45,35 +43,47 @@ import stockAPI.Transaction;
  - Per Stock: Current price | avg/mean buying price
 
  * Finished tasks
- - ~compile list of all stocks held~
- - ~After parsing all transactions, make sure that no nShares is negative for any stock~
+ - ~~compile list of all stocks held~~
+ - ~~After parsing all transactions, make sure that no nShares is negative for any stock~~
+ - ~~compile list of all stock held at this current moment~~
+ - ~~Connect to H2 database using JDBC, write & read stuff to it~~
+ - ~~Stream.java -> flattenResult~~
+ - ~~check whether txType and price (pos or neg) corresponds~~
  */
 
 @SuppressWarnings({"SqlNoDataSourceInspection", "SqlDialectInspection"})
 class MainTest {
   String path = "src/test/java/testdata.txt";
-  Result<FileReader> fR = FileReader.read(path);
+  String pathErrorFile = "src/test/java/testdata_error.txt";
+  Result<FileReader> fR;
   static LocalDate date = LocalDate.parse("2022-02-18");
-  @SuppressWarnings("unused")
-  static String txType = "SELL";
   static String symbol = "VTI";
   static int nShares = 10;
   static BigDecimal buyPrice = new BigDecimal("40.11");
 
   // Create in-memory database
-  String DB_INMEM = "jdbc:h2:mem:db0";
+  String DB_INMEM = "jdbc:h2:mem:";
 
   // SQL Strings
-  String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS transactions (id IDENTITY PRIMARY KEY, date TIMESTAMP, symbol VARCHAR, numShares INT, price NUMERIC)";
-  String SQL_INSERT_1 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-10-09 15:36:00', 'AVUV', 100, 40.00)";
-  String SQL_INSERT_2 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-10-12 15:00:00', 'VTI', 40, 50.11)";
-  String SQL_INSERT_3 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-10-12 16:00:00', 'AVUV', -40, 60.00)";
+  String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS transactions (id IDENTITY PRIMARY KEY, date DATE, symbol VARCHAR, numShares INT, price NUMERIC(20,3))";
+  String SQL_INSERT_1 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-02-18', 'VTI', 10, 40.11)";
+  String SQL_INSERT_2 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-10-09', 'AVUV', 100, 40.00)";
+  String SQL_INSERT_3 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-10-12', 'VTI', 40, 50.11)";
+  String SQL_INSERT_4 = "INSERT INTO transactions (date, symbol, numShares, price) VALUES ('2022-10-12', 'AVUV', -40, 60.00)";
   String SQL_QUERY = "SELECT date, symbol, numShares, price FROM transactions";
 
   @Test
   void parseTest() {
-    var stocks = fR.map(this::parseStocks);
-    stocks.map(this::checkNegativeStocks).forEach(System.out::println);
+    fR = FileReader.read(pathErrorFile);
+    Result<Map<Symbol, Integer>> res;
+
+    res = FileReader.read(path)
+        .map(this::parseStocks)
+        .flatMap(this::checkNegativeStocks);
+    assertTrue(res.isSuccess());
+    res.forEach(m -> m.get(Symbol.symbol("VXUS"))
+        .forEachOrFail(nShares -> assertEquals(334, nShares))
+        .forEach(Assertions::fail));
   }
 
   private Map<Symbol, Integer> parseStocks(FileReader input) {
@@ -86,8 +96,12 @@ class MainTest {
   private Result<Map<Symbol, Integer>> checkNegativeStocks(Map<Symbol, Integer> stocks) {
     List<Tuple<Symbol, Integer>> negativeStock = stocks.stream().filter(t -> t._2 < 0).toList();
     return negativeStock.isEmpty()
-        ? Result.success(Map.empty())
+        ? Result.success(stocks)
         : Result.failure("Input data contains stocks with negative number of shares: " + negativeStock.stream().map(t -> t._1).toList());
+  }
+
+  void printFailure(Object obj) {
+    System.out.println("Failed: " + obj.toString());
   }
 
   @Test
@@ -122,7 +136,6 @@ class MainTest {
             res.getString("symbol"),
             res.getInt("numShares"),
             res.getBigDecimal("price"));
-        System.out.println(tx);
       }
       res.close();
     } catch (SQLException e) {
@@ -133,34 +146,36 @@ class MainTest {
 
   @Test
   void parseIntoH2() {
-    var stocks = fR.map(this::parseStocks);
-//    stocks.map(this::checkNegativeStocks).forEach(System.out::println);
+    fR = FileReader.read(path);
+    Result<Map<Symbol, Integer>> stocks = fR.map(this::parseStocks);
 
     try (Connection db = DriverManager.getConnection(DB_INMEM);
         Statement statement = db.createStatement()) {
-//      DBWriter.executeStatement("create table if not exists transactions (id identity primary key, date timestamp, symbol varchar, numShares int, price numeric),",
-//          s);
-      statement.execute("create table if not exists transactions (id identity primary key, date timestamp, symbol varchar, numShares int, price numeric)");
-      statement.execute("insert into transactions (date, symbol, numShares, buyPrice) values ('2022-10-09 15:36:00', 'AVUV', 100, 40.00)");
+      statement.execute(SQL_CREATE_TABLE);
+      statement.execute(SQL_INSERT_1);
       ResultSet res = statement.executeQuery("select * from transactions");
       while (res.next()) {
         Transaction tx = Transaction.transaction(
             res.getObject("date", LocalDate.class),
             res.getString("symbol"),
             res.getInt("numShares"),
-            res.getBigDecimal("buyPrice"));
-        System.out.println(tx);
+            res.getBigDecimal("price"));
       }
       res.close();
     } catch (SQLException e) {
-      System.out.println("SQLException: " + e);
+      fail("SQLException: " + e);
     }
   }
 
   @Test
   void createTxTest() {
+    fR = FileReader.read(pathErrorFile);
+    assertTrue(fR.flatMap(Main::createTxWithType).isFailure());
+
     Transaction tx = Transaction.transaction(date, symbol, nShares, buyPrice);
-//    System.out.println(tx.toString());
-    fR.flatMap(Main::createTxWithType).forEach(t -> assertEquals(tx, t._1));
+    FileReader.read(path)
+        .flatMap(Main::createTxWithType)
+        .forEachOrFail(t -> assertEquals(tx, t._1))
+        .forEach(Assertions::fail);
   }
 }
