05e235d7a7af674325f0266bbef390179acaa951
Raphael Cottier <github.zing@dfgh.net>
2022-10-18 10:01:19 +0200

    Refactored methods to check order and correct secquence of input data into Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 import functionalUtilities.FileReader;
+import functionalUtilities.List;
 import functionalUtilities.Result;
 import functionalUtilities.Stream;
+import functionalUtilities.Tuple;
+import java.time.LocalDate;
+import java.util.function.Function;
 import stockAPI.Parser;
+import stockAPI.Transaction;
 
 public class Main {
 
@@ -26,5 +31,36 @@ public class Main {
         // if no args, show help
         // if only db.sqlite, show prompt
         // if db.sqlite & file, parse file and add transactions to db        try {
+    }
+
+    public static LocalDate getNextDate(List<Transaction> l) {
+        LocalDate first = l.head().getDate();
+        Function<LocalDate, Boolean> isNextDate = d -> first.compareTo(d) != 0;
+        return l.tail().foldLeftAbsorbAccPred(first, isNextDate, ignored -> Transaction::getDate);
+    }
+
+    // Default: true for ascending and all the same date
+    public static Tuple<Boolean, List<Transaction>> getOrderSeq(List<Transaction> l) {
+        LocalDate first = l.head().getDate();
+        Boolean res = l.isEmpty() || l.tail().isEmpty()
+            ? true
+            : getNextDate(l).isBefore(first)
+                ? false
+                : true;
+        return new Tuple<>(res, l);
+    }
+
+    public static Result<List<Transaction>> checkCorrectSequence(List<Transaction> l, boolean ASC) {
+        Function<Integer, Boolean> invertIfNecessary = ASC
+            ? i -> i >= 0
+            : i -> i <= 0;
+        Result<Tuple<LocalDate, Integer>> res = l.foldLeftAbsorbAccPred(
+            Result.success(new Tuple<>(l.head().getDate(), 0)),
+            Result::isFailure,
+            acc -> e -> acc.flatMap(t -> invertIfNecessary.apply(e.getDate().compareTo(t._1))
+                ? Result.success(new Tuple<>(e.getDate(), t._2 + 1))
+                : Result.<Tuple<LocalDate, Integer>>failure("Wrong date after line " + t._2)
+            ));
+        return res.map(ignored -> l);
     }
 }
