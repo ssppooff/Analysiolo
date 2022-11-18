adfe313bc8371269dd8bf16f13a9a3552995ad48
Raphael Cottier <github.zing@dfgh.net>
2022-11-18 13:52:04 +0100

    Implemented proper way to apply theme prior to rendering the results

--- src/main/java/Analysiolo/Utilities.java
+++ src/main/java/Analysiolo/Utilities.java
@@ -17,6 +17,7 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import stockAPI.DataSource;
@@ -25,6 +26,27 @@ import stockAPI.Symbol;
 import stockAPI.Transaction;
 
 final class Utilities {
+    private enum Theme {
+        Header ((cell, width) -> padCellToLen(cell, width, true)),
+        RowName ((cell, width) -> padCellToLen(cell, width, false)),
+        Data ((cell, width) -> padCellToLen(cell, width, true));
+
+        private final BiFunction<String, Integer, String> theme;
+
+        Theme(BiFunction<String, Integer, String> theme) {
+            this.theme = theme;
+        }
+
+        public String apply(String cell, int width) {
+            return theme.apply(cell, width);
+        }
+
+        public BiFunction<String, Integer, String> get() {
+            return theme;
+        }
+
+    }
+
     private Utilities() {}
 
     static String computeDate(final TimeFilter filter) {
@@ -201,6 +223,131 @@ final class Utilities {
         return Utilities.prettifyList(Utilities.parseTimeFilter(tf));
     }
 
+    static List<List<String>> applyTheme(Tuple<List<LocalDate>, Map<Symbol, List<BigDecimal>>> data,
+        Function<List<List<String>>, List<List<String>>> theme) {
+        List<String> header = List.list();
+        Map<Symbol, List<BigDecimal>> priceData = data._2;
+
+        if (data._1.size() > 1) {
+            priceData = priceData.mapVal(Utilities::addChangeMetrics);
+            header = List.of("𝝙", "𝝙 (%)");
+        }
+
+        header = data._1.reverse()
+                        .foldLeft(header, hd -> date -> hd.prepend(date.toString()))
+                        .prepend("");
+
+        List<List<String>> strData = priceData
+            .toList(sym -> prices -> prices
+                .map(priceBD -> String.format("%.3f", priceBD))
+                .prepend(sym.getSymbolStr()))
+            .prepend(header);
+
+        // Apply theme
+        return theme.apply(strData);
+    }
+
+    static Function<List<List<String>>, List<List<String>>> themeOnePrice() {
+        String horzSep = "-";
+        return s -> {
+            List<List<String>> res = prepocessData(s);
+            List<Integer> columnWidth = getColumnMaxWidth(s);
+
+            res = applyThemes(res, columnWidth, Theme.Header, Theme.RowName, Theme.Data);
+
+            // create & insert horiz rule
+            List<String> horzRule = res.head().map(hd -> horzSep.repeat(hd.length()));
+            return res.tail().prepend(horzRule).prepend(res.head());
+        };
+    }
+
+    static Function<List<List<String>>, List<List<String>>> themeTwoPricesWithDelta() {
+        String horzSep = "-";
+        String vertDateSep = "~>";
+        String vertResSep = "|";
+        return s -> {
+            List<List<String>> res = prepocessData(s);
+            List<Integer> columnWidth = getColumnMaxWidth(s);
+
+            res = applyThemes(res, columnWidth, Theme.Header, Theme.RowName, Theme.Data);
+
+            // create horiz rule with gap for first vert rule and '+' for second
+            // insert both vertical rules, not in the horiz rule, except for the "+"
+            int vertRulePos = 2;
+            int vertRulePos2 = 4;
+            List<String> horzRule = res.head().map(hd -> horzSep.repeat(hd.length()))
+                                       .insert(vertRulePos, " ".repeat(vertDateSep.length()))
+                                       .insert(vertRulePos2, "+".repeat(vertResSep.length()));
+
+            return res.tail().map(row -> row.insert(vertRulePos, vertDateSep)
+                                           .insert(vertRulePos2, vertResSep))
+                     .prepend(horzRule)
+                     .prepend(res.head()
+                                 .insert(vertRulePos, vertDateSep)
+                                 .insert(vertRulePos2, vertResSep));
+        };
+    }
+
+    private static List<List<String>> prepocessData(final List<List<String>> s) {
+        int maxRowSize = s.map(List::size).reduce(0, max -> len -> len > max ? len : max);
+        return padMissingCells(s, maxRowSize);
+    }
+
+    private static List<List<String>> applyThemes(List<List<String>> s, List<Integer> columnWidth,
+        Theme themeHeader,
+        Theme themeRowName,
+        Theme themeData) {
+        // You can change whether you want the top-left cell to either be styled
+        // according to the header or the row-name by first applying either one or the other
+        return s.map(row -> row.tail()
+                               .prepend(themeRowName.apply(row.head(), columnWidth.head())))
+                .tail().map(row -> List.zipWith(row, columnWidth, themeData.get()))
+                .prepend(List.zipWith(s.head(), columnWidth, themeHeader.get()));
+    }
+
+    private static String padCellToLen(String cell, int length, boolean rightJustified) {
+        String padding = " ".repeat(length - cell.length());
+        return rightJustified
+            ? padding + cell
+            : cell + padding;
+    }
+
+    static List<List<String>> padMissingCells(final List<List<String>> rowData, int maxRowSize) {
+        return rowData.map(row -> {
+            int padLength = maxRowSize - row.size();
+            if (padLength > 0)
+                row = List.concat(row, Stream.repeat("").take(padLength).toList());
+            return row;
+        });
+    }
+
+    static List<Integer> getColumnMaxWidth(final List<List<String>> rowData) {
+        return rowData.map(row -> row.map(String::length))
+                                            .reduce(row1 -> row2 -> row1.zipWith(row2, Math::max));
+    }
+
+    static Tuple<List<LocalDate>, Map<Symbol, List<BigDecimal>>> processData(
+        List<Tuple<LocalDate, List<Tuple<Symbol, BigDecimal>>>> data) {
+        List<LocalDate> dates = data.map(Tuple::_1);
+
+        // TODO data.stream()
+        List<Tuple3<Symbol, LocalDate, BigDecimal>> flattenedData =
+            data.foldLeft(List.list(), resList -> outerT ->
+                outerT._2.foldLeft(resList, innerResList -> innerT ->
+                    innerResList.prepend(new Tuple3<>(innerT._1, outerT._1, innerT._2))));
+
+        Map<Symbol, List<BigDecimal>> priceData =
+            flattenedData.groupBy(Tuple3::_1)
+                         .mapVal(lT3 -> lT3
+                             .sortFP(Comparator.comparing(Tuple3::_2))
+                             .map(Tuple3::_3));
+
+        if (dates.size() >= 1)
+            priceData = priceData.mapVal(Utilities::addChangeMetrics);
+
+        return new Tuple<>(dates, priceData);
+    }
+
     static List<List<String>> formatDataWithHeader(List<Tuple<LocalDate, List<Tuple<Symbol, BigDecimal>>>> data) {
         List<LocalDate> dates = data.map(Tuple::_1);
         List<String> header = dates.size() >= 1
@@ -239,12 +386,6 @@ final class Utilities {
         return List.of(origPrice, newPrice, abs, percent);
     }
 
-    static List<List<String>> addWithSeparator(final List<String> header, final List<List<String>> data) {
-        String separator = "-";
-        List<String> rule = header.map(hd -> separator.repeat(hd.length()));
-        return data.prepend(rule).prepend(header);
-    }
-
     static String renderTable(List<List<String>> data) {
         return renderTable(data.map(List::toArray).toArray());
     }
@@ -261,7 +402,7 @@ final class Utilities {
     }
 
     static List<List<String>> padCells(final List<List<String>> data) {
-    return padCells(data, true);
+        return padCells(data, true);
     }
 
     static List<List<String>> padCells(final List<List<String>> data, boolean rightJustified) {
